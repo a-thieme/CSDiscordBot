7bcfdd52522e2b5f8e6780a4f42d219e6415cb1d
a-thieme <adam.thieme@gmail.com>
2021-08-30 15:01:30 -0500

    more input code

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import json
 import discord
 
+global master_dict
+
 
 class MyClient(discord.Client):
+    bot_indicator = "!cs"
+
     async def on_ready(self):
         print('Logged on as', self.user)
 
@@ -11,12 +15,16 @@ class MyClient(discord.Client):
             # Ignore bot comments
             return
 
+        if "!cs" in message.content:
+            process_input(message.content)
         if message.content == 'ping':
             await message.channel.send('Pong! {0}ms'.format(round(client.latency, 3)))
 
 
+def process_input(message):
+    splitted = message.split(" ")
 
-global master_dict
+    return
 
 
 def get_prof(name):
