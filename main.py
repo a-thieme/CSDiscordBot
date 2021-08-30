56a63f158b5b311f67425ac15ad969de1101aa20
Marshall Lussier <marshallelussier@gmail.com>
2021-08-30 16:06:43 -0500

    Fixed code for classes command

--- main.py
+++ main.py
@@ -14,23 +14,29 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
 
-        if "!cs" in message.content:
-            process_input(message.content)
+        if "~cs" in message.content:
+            await message.channel.send(process_input(message))
         if message.content == 'ping':
             await message.channel.send('Pong! {0}ms'.format(round(client.latency, 3)))
-        if message.content == '~classes':
-            course_message = discord.Embed(title="Computer Science (Major) Courses", description="A list of the required courses for the CS Major", color=0x184b91)
-            for course in master_dict["Courses"]:
-                course_message.add_field(name=course, value="Value1", inline=True)
-                for courseinfo in master_dict["Courses"]:
-                    print(courseinfo[0])
-            await message.channel.send(embed=course_message)
 
 
 def process_input(message):
-    splitted = message.split(" ")
+    split = message.content.strip().lower().split(" ")
+    leading = split[1]
+    if leading == "info":
+        if len(split) == 2:
+            return "length is 2"
+    #        cs info
+    elif leading == "classes":
+        course_message = discord.Embed(title="Computer Science (Major) Courses", description="A list of the required courses for the CS Major", color=0x184b91)
+        for course in master_dict["Courses"]:
+            for courseinfo in master_dict["Courses"]:
+                course_message.add_field(name=course, value=master_dict["Courses"][course]['name'], inline=True)
+                return course_message
+    elif leading == "professors":
+        return "leading is professors"
 
-    return
+    return "error happened"
 
 
 def get_prof(name):
@@ -38,6 +44,19 @@ def get_prof(name):
         if name.lower() in professor.lower():
             for key in master_dict["Professors"][professor]:
                 print(master_dict["Professors"][professor][key])
+
+
+def reformat_class_name(name):
+    new_name = ""
+    temp = name.split('-')
+    new_name += temp[0] + temp[1]
+    if len(temp) > 2:
+        new_name += '-' + temp[2]
+    return new_name
+
+
+def get_class(class_name):
+    return "something"
 
 
 if __name__ == "__main__":
@@ -48,6 +67,4 @@ if __name__ == "__main__":
     # start discord bot
     client = MyClient()
     client.run('ODQxODkxMTk2MjA0NjEzNzAy.YJtWRg.KEPdDGnMts7b3EvRnVYIUCVXeI0')
-
-    # testing
-    get_prof("Santosh")
+    
