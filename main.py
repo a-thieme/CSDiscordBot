c614a05fe99ba8b35094cd562184b2fbd50a9c55
a-thieme <adam.thieme@gmail.com>
2021-08-30 14:55:17 -0500

    added discord bot skeleton code

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import json
+import discord
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            # Ignore bot comments
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('Pong! {0}ms'.format(round(client.latency, 3)))
+
+
 
 global master_dict
 
@@ -11,7 +27,13 @@ def get_prof(name):
 
 
 if __name__ == "__main__":
+    # import info
     input_file = open("updated.json")
     master_dict = json.load(input_file)
 
+    # start discord bot
+    client = MyClient()
+    client.run('ODQxODkxMTk2MjA0NjEzNzAy.YJtWRg.O7fSXIonBDCKgy5ASSUel9Nr1KQ')
+
+    # testing
     get_prof("Santosh")
