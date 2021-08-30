e2afc8e5254a96850d7b513fd93ac419839b56fc
a-thieme <adam.thieme@gmail.com>
2021-08-30 15:29:05 -0500

    updated

--- main.py
+++ main.py
@@ -16,13 +16,24 @@ class MyClient(discord.Client):
             return
 
         if "!cs" in message.content:
-            process_input(message.content)
+            process_input(message)
         if message.content == 'ping':
             await message.channel.send('Pong! {0}ms'.format(round(client.latency, 3)))
 
 
 def process_input(message):
-    splitted = message.split(" ")
+    split = message.content.strip().lower().split(" ")
+    leading = split[1]
+    if leading == "info":
+        if len(split) == 2:
+
+    #        cs info
+
+
+    elif leading == "classes":
+
+    elif leading == "professors":
+        return
 
     return
 
@@ -32,6 +43,18 @@ def get_prof(name):
         if name.lower() in professor.lower():
             for key in master_dict["Professors"][professor]:
                 print(master_dict["Professors"][professor][key])
+
+def reformat_class_name(name):
+    new_name = ""
+    temp = name.split('-')
+    new_name += temp[0] + temp[1]
+    if len(temp) > 2:
+        new_name += '-' + temp[2]
+    return new_name
+
+def get_class(class_name):
+    class_name = reformat_class_name(class_name)
+    if class_name in master_dict["Courses"]:
 
 
 if __name__ == "__main__":
