a4bd540b95c3b9ccb1fba897e62c280825cf8fbe
a-thieme <adam.thieme@gmail.com>
2021-08-30 14:52:23 -0500

    add functionality for getting professor info

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import json
 
+global master_dict
+
+
+def get_prof(name):
+    for professor in master_dict["Professors"]:
+        if name.lower() in professor.lower():
+            for key in master_dict["Professors"][professor]:
+                print(master_dict["Professors"][professor][key])
+
+
 if __name__ == "__main__":
     input_file = open("updated.json")
     master_dict = json.load(input_file)
+
+    get_prof("Santosh")
