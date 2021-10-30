c4017a2266a5d24dbbdfc69e22a172436a496c8a
BuildTools <unconfigured@null.spigotmc.org>
2021-10-29 22:28:05 -0500

    Quick fix for id data types

--- core/CommandEvent.py
+++ core/CommandEvent.py
@@ -25,7 +25,7 @@ class CommandEvent:
         if self.command.mod_command and self.message.author.id not in self.bot.admins:
             embed_builder.description = "This command is only for bot admins"
             await self.message.channel.send(embed=embed_builder)
-            return  # mod command check
+            return  # admin command check
         if self.command.required_role is not None:
             role = discord.utils.find(lambda r: r.name == self.command.required_role, self.message.guild.roles)
             if role not in self.message.author.roles:
--- main.py
+++ main.py
@@ -30,8 +30,8 @@ class CSBot(discord.Client):
             return
         if isinstance(message.channel, DMChannel):
             return
-        if message.content.lower().startswith("~cs "):
-            args = message.content.lower().replace("~cs ", "", 1).split(" ")
+        if message.content.lower().startswith("#cs "):
+            args = message.content.lower().replace("#cs ", "", 1).split(" ")
             cmd = args[0]
             args.pop(0)
             locate_command = find_command(cmd, self)
